28e91da396b3356b95eebcec6bbdc6ddfddac2c1
lyl5380@bu.edu <770483011@qq.com>
2018-11-20 03:18:48 -0500

    learn const type

---
+++ ConstAndLet/ConstAndLet.nim
@@ -0,0 +1,14 @@
+const
+    x = 2089
+    y = 1995
+    period = x-y
+var name = "nobody"
+echo "your lover is ", name, "."
+echo "you might stay in this status for ", x-y, "years."
+echo "so, tell me who is your soul mate right now"
+name = readLine(stdin)
+echo "when did you start the relationship?"
+var now = readLine(stdin)
+echo "your lover is ", name, "."
+echo "you might stay in this status form ", now, "to ", x
+let input = readline(stdin) # help to keep display
